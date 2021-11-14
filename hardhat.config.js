52709f486a7c9c5a16743f59a8ad4d9c224f664c
Bertrand Juglas <b@juglas.name>
2021-11-14 20:37:53 +0100

    feat: adding networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -52,12 +52,29 @@ module.exports = {
       chainId: 3,
       accounts: real_accounts,
     },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`,
+      tags: ["test", "legacy", "use_root"],
+      chainId: 4,
+      accounts: real_accounts,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_ID}`,
+      tags: ["test", "legacy", "use_root"],
+      chainId: 42,
+      accounts: real_accounts,
+    },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
       tags: ["legacy", "use_root"],
       chainId: 1,
       accounts: real_accounts,
     }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY
   },
   mocha: {
   },
